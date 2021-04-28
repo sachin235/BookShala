1fa054afa85c8a598145e99ee1fb00666ea6b69a
sachin235 <sachin23hisar@gmail.com>
2021-04-29 02:05:51 +0530

    Update Product Preview

--- public/js/product.js
+++ public/js/product.js
@@ -8,44 +8,24 @@ function loadProductsbyId() {
 		console.log(products);
 
 		for (let p of products) {
-			$("#products-container").append(
-				$(`
-          <center>
-          <div class="col-4">
-            <div class="card m-2"> 
-            <a href="/${p.id}/product">
-            <img src="${p.avatar}" class="card-img-top">
-            </a>
-              <div class="card-body">
-                <h5 class="card-title">${p.name}</h5>
-                <p class="card-text">
-                  ${p.company}
-                </p>
-                <h6 class="card-subtitle mb-2 text-muted">Rs. ${p.price}</h6>
-                <h6 class="card-subtitle mb-2 text-muted">${p.username}</h6>
-                </div>
-            </div>
-          </div>
-          </center>
-          `)
-			);
-
 			$("#container-product").append(
 				$(`
-          <div><img src="${p.avatar}"></div>
-          </div>
-          <div>
-            <h2>Book Details</h2>
-            <div class="card-body">
-              <h4 class="card-subtitle mb-2">Product Id: ${p.id}</h4>  
-              <h4 class="card-subtitle mb-2">Book Name: ${p.name}</h4>
-              <h4 class="card-subtitle mb-2">Author/Company: ${p.company}</h4>
-              <h4 class="card-subtitle mb-2">Price: Rs. ${p.price}</h4>
-              <h4 class="card-subtitle mb-2">Seller Username: ${p.username}</h4>
-                Login in to Your Account to contact the Seller via CHAT ROOM
+          <center>
+            <div class="col-6"><img src="${p.avatar}" width="500" height="600"></div>
+
+            <div>
+              <h2>Book Details</h2>
+              <div class="card-body">
+                <h4 class="card-subtitle mb-2">Product Id: ${p.id}</h4>  
+                <h4 class="card-subtitle mb-2">Book Name: ${p.name}</h4>
+                <h4 class="card-subtitle mb-2">Author/Company: ${p.company}</h4>
+                <h4 class="card-subtitle mb-2">Price: Rs. ${p.price}</h4>
+                <h4 class="card-subtitle mb-2">Seller Username: ${p.username}</h4>
+                  Login in to Your Account to contact the Seller via CHAT ROOM
+              </div>
             </div>
-          </div>
-          `)
+          </center>
+        `)
 			);
 		}
 	});
